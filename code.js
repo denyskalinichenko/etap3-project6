e331493efb402919b4b174eefa75754c3fd66250
Denys <44090130+denyskalinichenko@users.noreply.github.com>
2020-11-13 20:13:33 +0100

    Add files via upload
    version advanced (different messages, different passwords)

--- code.js
+++ code.js
@@ -1,37 +1,24 @@
 const input = document.getElementById('pass');
 const div = document.querySelector('.message')
-const password = "admin";
-const message = "Wyjechałem po wprowadzeniu hasła";
+const password = ["admin", "denis", "olga"];
+const message = ["ADMIN", "DENIS", "Olha"];
 
-// -------"keyup"-------- kiedy klawisza jest naciśnięta i póżniej podniesiona do góry (zwykłe naciśnięcia)
-// input.addEventListener("keyup", (e) =>{
-//     console.log(e.target.value)
-// }) 
-
-// ------"change"-------- jak tylko wychodzimy z inputa - wtedy pokazuje wartość
-// input.addEventListener("change", (e) =>{
-//     console.log(e.target.value);
-// })
-
-
-// -------"input"------bardziej pasuje bo odpowiada za zmianę w inpucie
-input.addEventListener("input", (e) =>{
-    if(e.target.value == password){
-        div.textContent = message;
-        // jeżeli hasło wprowadzone - odrazu czyszczimy go:
-        e.target.value = '';
-    } else {
-        div.textContent = '';
-    }
+// wprowadzenia hasła (jednego z tablicy "password" - pokazuje wiadomość("message") która pasuję do indeksu "password")
+input.addEventListener("input", (e) => {
+    div.textContent = '';
+    const text = e.target.value;
+    password.forEach((pswrd, index) => {
+        if (pswrd === text) {
+            div.textContent = message[index];
+            e.target.value = '';
+        }
+    })
 })
 
-// jezeli kliknę w input - doda klasę do input'a
 input.addEventListener("focus", (e) => {
     e.target.classList.toggle("active");
 })
 
-// jezeli kliknę w input - zabierze klasę od input'a
 input.addEventListener("blur", (e) => {
     e.target.classList.toggle("active");
 })
-
